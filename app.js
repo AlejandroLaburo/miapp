9115253b55fd88c5e9cc3766179f57b4a9d529cf
Matoskelvin <kelvinvmatos2@msil.com>
2025-07-17 15:21:51 -0400

    add function edit

---
+++ app.js
@@ -0,0 +1,23 @@
+const formulario = document.querySelector(".form-create");
+let cont = 1;
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const descripcion = document.querySelector("#descripcion").value;
+    const contenedor = document.querySelector(".publicaciones");
+    contenedor.innerHTML += `<div id="${cont}" class="publicacion">
+            <p>id:${cont} </p>
+            <p>${descripcion}</p>
+            <button id="editar" onclick="editar(this)">Editar</button>
+            <button id="eliminar" onclick="eliminar(this)">Eliminar</button>
+        </div>`;
+    cont++;
+});
+
+
+function editar(e){
+
+}
+
+function eliminar(e){
+    e.parentElement.remove("p:nth-child(2)");
+}
